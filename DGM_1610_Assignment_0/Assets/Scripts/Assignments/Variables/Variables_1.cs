1c2bab6db2df3c90a44881958eba8966832d6bd7
Carter <clgaspect@gmail.com>
2020-01-13 17:14:31 -0700

    Added folders and scripts
    Added folders for scripts in Unity, Created one new C# script and added a variable to the script.

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/Variables_1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables_1 : MonoBehaviour
+{
+    // Declare a variable AND initialize it. Declaring and Initializing are separate actions, even if performed in the same line of code.
+    int number = 17;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
