cab8f979363408edb2a8d74f94d468de0d36f738
Carter <clgaspect@gmail.com>
2020-01-15 17:17:41 -0700

    Added variables and functions
    Created some functions in class to calculate simple math. Had to delete color changing script.

--- DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/Variables_1.cs
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/Variables_1.cs
@@ -5,28 +5,32 @@ using UnityEngine;
 public class Variables_1 : MonoBehaviour
 {
     // Declare a variable AND initialize it. Declaring and Initializing are separate actions, even if performed in the same line of code.
-    int number = 17;
-    
+
+    public int amount;
+    public float money;
+    public float cost;
+
     // Start is called before the first frame update
     void Start()
     {
+        Calculate(amount, money, cost);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            GetComponent<Renderer>().material.color = Color.red;
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GetComponent<Renderer>().material.color = Color.green;
-        }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            GetComponent<Renderer>().material.color = Color.blue;
-        }
+
+    }
+
+    void Calculate(int amount, float money, float cost)
+    {
+        float total;
+        total = money - amount * cost;
+        print(total);
+    }
+    void CheckScore()
+    {
+        print("Sup Dawgz");
     }
 }
