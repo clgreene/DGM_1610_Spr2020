7880f87fbb0ecf28f08f512168c8e097b676fd7d
Carter <clgaspect@gmail.com>
2020-01-17 13:19:19 -0700

    Added loop scripts
    Practiced with while loops and if statements.

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    int enemies = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (enemies > 0)
+        {
+            print("Master chief kills an enemy.");
+            enemies--;
+        }
+
+        if (enemies == 0)
+        {
+            print("The Master Chief has killed all the covenant.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
