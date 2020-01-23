9210602d5f6f9e6861ab4dc629238d85ec7a6127
Carter <clgaspect@gmail.com>
2020-01-22 17:16:26 -0700

    First Person Action BOI
    Added movement, collision, camera child relations, and rotation.

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/CubeMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    float moveSpd = 0.1f;
+    float lookSpd = 2.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpd);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpd, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -moveSpd);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpd, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -lookSpd, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, lookSpd, 0);
+        }
+
+
+    }
+
+    // Detect collision with another object.
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
