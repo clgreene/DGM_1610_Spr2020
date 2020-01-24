36055e01fd51f91601aad2f73175b782318a9fb3
Carter <clgaspect@gmail.com>
2020-01-23 18:01:57 -0700

    Added wasd movement
    added wasd movement to new scene

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/WASDMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovement : MonoBehaviour
+{
+    float moveSpd = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpd);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpd, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -moveSpd);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpd, 0, 0);
+        }
+    }
+}
