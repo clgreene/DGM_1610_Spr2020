c2d56526ba81783a9430ce1c2be8dc29b9d30a94
Carter <clgaspect@gmail.com>
2020-01-27 12:39:54 -0700

    Added more to Endless Runner
    Added Fog, obstacle scripts and obstacles, refined movement, extended level.

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Youtube Tutorials/Collisions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Enemy")
+        {
+            movement.enabled = false;
+        }
+    }
+}
--- DGM_1610_Assignment_0/Assets/Scripts/Youtube Tutorials/PlayerMovement.cs
+++ DGM_1610_Assignment_0/Assets/Scripts/Youtube Tutorials/PlayerMovement.cs
@@ -6,23 +6,23 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    public float playerSpeed = 0f;
-    public float playerStrafe = 500f;
+    public float playerSpeed = 1000f;
+    public float playerStrafe = 60f;
 
     // Start is called before the first frame update
     // Update is called once per frame
     void FixedUpdate()
     {
-        //rb.AddForce(0, 0, playerSpeed * Time.deltaTime);
+        rb.AddForce(0, 0, playerSpeed * Time.deltaTime);
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(playerStrafe * Time.deltaTime, 0, 0);
+            rb.AddForce(playerStrafe * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-playerStrafe * Time.deltaTime, 0, 0);
+            rb.AddForce(-playerStrafe * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
     
