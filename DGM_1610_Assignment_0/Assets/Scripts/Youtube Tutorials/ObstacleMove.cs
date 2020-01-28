8dfc4704513ee653604611f416c523b5c2819454
Carter <clgaspect@gmail.com>
2020-01-27 17:16:06 -0700

    Added a moving block
    Added a moving block to endless runner

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Youtube Tutorials/ObstacleMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    public Transform obstacle;
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obstacle.position.x > 4)
+        {
+            rb.AddForce(-300 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (obstacle.position.x < -4)
+        {
+            rb.AddForce(300 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
