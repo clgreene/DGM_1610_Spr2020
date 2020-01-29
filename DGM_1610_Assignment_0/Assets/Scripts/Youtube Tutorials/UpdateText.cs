1e825bbe3d9c7a86e093634a86bdf78a87ae9826
Carter <clgaspect@gmail.com>
2020-01-28 22:58:31 -0700

    Started creating a UI
    Began making a UI for Score Tracking. Failed because Unity doesn't have the functions that are used in the tutorial I'm following... I'll need to research a little and find out how it has changed.

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Youtube Tutorials/UpdateText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateText : MonoBehaviour
+{
+
+    public Transform player;
+    public Tex scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z;
+    }
+}
