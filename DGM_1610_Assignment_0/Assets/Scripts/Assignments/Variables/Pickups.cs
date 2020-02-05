1cffe514f325730133c28ba9599e117d04e3adeb
Carter <clgaspect@gmail.com>
2020-02-05 16:56:04 -0700

    Pushing partway through class adding pickups
    pickups

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/Pickups.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+
+    public string pickupName;
+    public string pickupType;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.AddPoints(score);
+            Destroy(gameObject);
+        }
+    }
+}
