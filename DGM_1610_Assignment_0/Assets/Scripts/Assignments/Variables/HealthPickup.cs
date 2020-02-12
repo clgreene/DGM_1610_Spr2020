d41edacced1dd00a180fdc2aa3b6645ff114c6a3
Carter <clgaspect@gmail.com>
2020-02-12 16:52:11 -0700

    Created Health Pickups

---
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : Pickups
+{
+
+    public int healthAmt = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Health: " + healthAmt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("You Gained " + healthAmt + " health!");
+    }
+}
