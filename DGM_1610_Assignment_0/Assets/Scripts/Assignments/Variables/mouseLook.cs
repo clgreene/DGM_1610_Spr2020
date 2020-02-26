168547a73c6926d980d49320ba055ea7d16242b4
Carter <clgaspect@gmail.com>
2020-02-26 13:54:27 -0700

    Thought up simple game idea, began creating it.
    Trouble shot some third-person controller issues until it was right.

--- DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/mouseLook.cs
+++ DGM_1610_Assignment_0/Assets/Scripts/Assignments/Variables/mouseLook.cs
@@ -23,7 +23,7 @@ public class mouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+        xRotation = Mathf.Clamp(xRotation, -15f, 20f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
