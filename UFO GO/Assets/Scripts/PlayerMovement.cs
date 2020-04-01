0dfba99b5f444a25fc6c7c86f386bd1a789b1e53
Carter <clgaspect@gmail.com>
2020-04-01 16:56:14 -0600

    Adding in elements for final game, tweaking movement.

--- UFO GO/Assets/Scripts/PlayerMovement.cs
+++ UFO GO/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    public float forwardForce = 800f;
-    public float strafe = 9000f;
-    public Transform player;
+    public float forwardForce = 900f;
+    public float strafe = 90f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +22,13 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            
+            rb.AddForce(strafe * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-strafe * Time.deltaTime, 0, 0);
+            rb.AddForce(-strafe * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
         }
     }
