ee25724cf071fe789288a101587d5128dc93946e
Carter <clgaspect@gmail.com>
2020-04-13 13:16:23 -0600

    added UI and Text elements, pickup scripts, fuel pickup obj

---
+++ UFO GO/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager: MonoBehaviour
+{
+
+    public static int score;
+    public static int health = 100;
+    public static int ammo = 10;
+    public static int fuel = 0;
+
+    void Update()
+    {
+        
+    }
+
+    public static void AddPoints(int value)
+    {
+        score += value;
+        Debug.Log("Game Score: " + score);
+    }
+
+    public static void AddHealth(int value)
+    {
+        health += value;
+        Debug.Log("Health:" + health);
+    }
+
+    public static void AddAmmo(int value)
+    {
+        ammo += value;
+        Debug.Log("Ammo: " + ammo);
+    }
+
+    public static void AddFuel(int value)
+    {
+        fuel += value;
+        Debug.Log("Fuel: " + fuel);
+    }
+}
---
+++ UFO GO/Assets/Scripts/Managers/UpdateText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour
+{
+
+    public Text fuelText;
+
+    public Text scoreText;
+
+    public Text healthText;
+
+    public Text ammoText;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuelText.text = ScoreManager.fuel.ToString("0");
+        scoreText.text = ScoreManager.score.ToString("0");
+        healthText.text = ScoreManager.health.ToString("0");
+        ammoText.text = ScoreManager.ammo.ToString("0");
+    }
+}
