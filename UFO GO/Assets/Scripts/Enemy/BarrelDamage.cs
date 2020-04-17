dc2dad81048fef14b375c7b44722c2c246dc29ef
Carter <clgaspect@gmail.com>
2020-04-17 12:00:07 -0600

    creating player health and damage scripts

---
+++ UFO GO/Assets/Scripts/Enemy/BarrelDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelDamage : MonoBehaviour
+{
+
+    public int value;
+    public int hit = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.tag == "Player") && (hit == 0))
+        {
+            ScoreManager.AddHealth(value);
+            hit = 1;
+        }
+
+    }
+
+}
