15c3941924a19dd4d7d9fe26c5fef6ed4f3be009
Carter <clgaspect@gmail.com>
2020-04-20 17:53:27 -0600

    Enemy move script down

---
+++ UFO GO/Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public Rigidbody rbfollow;
+    public int distance;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(rbfollow.worldCenterOfMass.x, 2, distance);
+    }
+}
