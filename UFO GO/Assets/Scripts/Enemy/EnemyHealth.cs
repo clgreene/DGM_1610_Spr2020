1f1a65502bcf53cdb71b53640d26ce8c33c100b8
Carter <clgaspect@gmail.com>
2020-04-21 13:26:10 -0600

    enemy health and death scripts

---
+++ UFO GO/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+
+            health = health - 10;
+            Destroy(other.gameObject);
+            if (health == 0) Destroy(gameObject);
+
+        }
+    }
+}
